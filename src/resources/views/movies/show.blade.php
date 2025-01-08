fbf1e5319279e830335f7d122a6ef4cb0eec1dd6
Kristiyan <kristiyan_hristov04@abv.bg>
2025-01-08 21:04:31 +0200

    Fixed showing edit and delete buttons on preview when current user is the creator of movie

--- src/resources/views/movies/show.blade.php
+++ src/resources/views/movies/show.blade.php
@@ -24,14 +24,14 @@
                         Език: {{ $movie->language }}
                     </p>
                     @if (Auth::user())
-                        @if (Auth::user()->id == $movie->id)
+                        @if (Auth::user()->id == $movie->user_id)
                             <a href="{{ route('movies.edit', $movie->id) }}" type="button"
                                 class="inline-block mt-6 px-5 py-2.5 rounded-lg text-white text-sm tracking-wider border-none outline-none bg-orange-600 hover:bg-orange-700 hover:cursor-pointer active:bg-orange-600">
                                 Редактирай
                             </a>
                         @endif
 
-                        @if (Auth::user()->id == $movie->id)
+                        @if (Auth::user()->id == $movie->user_id)
                             <form action="{{ route('movies.delete', $movie->id) }}" method="POST">
                                 @csrf
                                 @method('delete')
