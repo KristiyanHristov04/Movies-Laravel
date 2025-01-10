88fea9ca24f705a378d99202ae90881215558531
Kristiyan <kristiyan_hristov04@abv.bg>
2025-01-10 21:56:43 +0200

    Changed edit button styles

--- src/resources/views/movies/index.blade.php
+++ src/resources/views/movies/index.blade.php
@@ -85,7 +85,7 @@
                             @if (Auth::user())
                                 @if (Auth::user()->id == $movie->user_id)
                                     <a href="{{ route('movies.edit', $movie->id) }}" type="button"
-                                        class="inline-block mt-6 px-5 py-2.5 rounded-lg text-white text-sm tracking-wider border-none outline-none bg-orange-300 hover:bg-orange-400 hover:cursor-pointer active:bg-orange-400">
+                                        class="inline-block mt-6 px-5 py-2.5 rounded-lg text-white text-sm tracking-wider border-none outline-none bg-purple-600 hover:bg-purple-700 hover:cursor-pointer active:bg-purple-700">
                                         Редактиране
                                     </a>
                                 @endif
