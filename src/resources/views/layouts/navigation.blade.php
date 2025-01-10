769369e452f7f9869d91d60f596405dfe7cd887d
Kristiyan <kristiyan_hristov04@abv.bg>
2025-01-10 15:26:39 +0200

    Added logo

--- src/resources/views/layouts/navigation.blade.php
+++ src/resources/views/layouts/navigation.blade.php
@@ -1,12 +1,15 @@
 <nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
     <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="max-w-7xl mx-auto px-4 py-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
-                <div class="shrink-0 flex items-center">
-                    <a href="{{ route('movies.index') }}">
-                        FilmVusion
+                <div class="shrink-0 flex items-center gap-2">
+                    <a href="{{ route('movies.index') }}" class="h-[100%]">
+                        <img src="{{ asset('logo.png') }}" class="h-[100%]" />
+                    </a>
+                    <a href="{{ route('movies.index') }}" style="letter-spacing: 5px; color: #ff7600; font-family: sans-serif; font-weight: bold;">
+                        ФИЛМИ.БГ
                     </a>
                 </div>
 
